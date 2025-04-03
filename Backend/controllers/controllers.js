10f34194bc8f8acaab406195e042777144fbfada
neil761 <alvarezneilandrew486@gmail.com>
2025-04-03 11:12:58 +0800

    gg

--- Backend/controllers/controllers.js
+++ Backend/controllers/controllers.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose")
 
 
 const createBill = async(req, res) =>{
-    const {customer_id, payment_amount, payment_method, payment_date} = req.bod
+    const {customer_id, payment_amount, payment_method, payment_date} = req.body
 
     try{
         const bill = await Bill.create({customer_id, payment_amount, payment_method, payment_date})
         res.status(200).json(bill)
-    }catch{
-        res.status(400).json({error: error.message})
+    }catch (error){
+        res.status(400).json({ error: error.message })
     }
 }
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const routes = require('./routes/routes')
 
 const app = express()
+
+app.use(express.json());
+
+app.use("/api", routes)
 
 mongoose.connect('mongodb://localhost:27017/halooo')
 
