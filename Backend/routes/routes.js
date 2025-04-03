e248864c38848090b4225289725c547b43a9ea2f
Bautista-tine <cristinejoybautista544@gmail.com>
2025-04-03 11:52:00 +0800

    Cristine Joy C Bautista Function

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const {
     createBill,
 } = require ("../controllers/controllers")
+const {
+    register
+} = require('../controllers/Customercontrollers')
+
+router.post("/new", register)
 
 router.post("/customers/:id/paybill", async (req, res) => {
     req.body.customer_id = req.params.id;
