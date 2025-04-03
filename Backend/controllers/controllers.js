1be27eacab81d3531cca16269f357a5d48f97455
neil761 <alvarezneilandrew486@gmail.com>
2025-04-03 09:43:36 +0800

    hjm

--- Backend/controllers/controllers.js
+++ Backend/controllers/controllers.js
@@ -1,2 +1,7 @@
-const customer = require("../models/customers")
+const Customer = require("../models/customers")
+const Bill = require("../models/bill")
 const mongoose = require("mongoose")
+
+const createBill = async(req, res) =>{
+    const {customer_id, payment_amount, payment_method, payment_date} = req.body
+}
