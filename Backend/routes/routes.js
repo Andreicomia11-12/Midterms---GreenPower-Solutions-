6dd7102d593ee5a8796f975e38e640a546e92372
Margiecutie <margiecute09@gmail.com>
2025-04-03 11:58:57 +0800

    gg

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const router = express.Router();
 const {
+    getBill,
     createBill,
 } = require ("../controllers/controllers")
+
+router.get("/customers/:id/bill", getBill)
 
 router.post("/customers/:id/paybill", async (req, res) => {
     req.body.customer_id = req.params.id;
